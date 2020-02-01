09e4d67f3f165ecba18d8e7681ab2f6e1d2976a3
wlaub <goldcray@gmail.com>
2020-02-01 12:48:21 -0800

    Updated CPLD project files

--- PrometheusII/CPLD/main.vhd
+++ PrometheusII/CPLD/main.vhd
@@ -30,35 +30,80 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity main is
-    Port ( CLK : in  STD_LOGIC;
-           INPUT : in  STD_LOGIC;
-           OUTPUT : out  STD_LOGIC;
-           TAPS : in  STD_LOGIC_VECTOR (15 downto 0));
+    Port ( 
+	        CLK1 : in  STD_LOGIC;
+           INPUT1 : in  STD_LOGIC;
+           OUTPUT1 : out  STD_LOGIC;
+			  OE1 : in  STD_LOGIC;
+			  
+			  EXT_CLK : in STD_LOGIC;
+			  CLK_SEL : in STD_LOGIC;
+
+			  CLK2 : in  STD_LOGIC;
+           INPUT2 : in  STD_LOGIC;
+           OUTPUT2 : out  STD_LOGIC;
+			  OE2 : in  STD_LOGIC;		  
+			  
+			  SCK : in STD_LOGIC;
+			  SDATA : in STD_LOGIC
+           );			  
 end main;
 
 
 architecture Behavioral of main is
-	signal reg: std_logic_vector(15 downto 0);
-	signal fb: STD_LOGIC;
+	signal reg1: std_logic_vector(15 downto 0);
+	signal reg2: std_logic_vector(15 downto 0);
+	signal tap_reg: std_logic_vector(15 downto 0);
+	signal fb1: STD_LOGIC;
+	signal fb2: STD_LOGIC;
+	signal int_clk1: STD_LOGIC;
 begin
 
-	process(reg, INPUT, TAPS) is
+	
+	process(reg1, INPUT1, tap_reg) is
 		variable tmp : std_logic;
 	begin
-	 tmp := INPUT;
+	 tmp := INPUT1;
     for I in 15 downto 0 loop
-        tmp := tmp xor (reg(I) and TAPS(I));
+        tmp := tmp xor (reg1(I) and tap_reg(I));
     end loop;
-    fb <= tmp;
+    fb1 <= tmp;
 	end process;
 				
-	process (CLK)
+   int_clk1 <= CLK1 when CLK_SEL = '0' else EXT_CLK;
+	process (int_clk1)
 	begin
-		if CLK'event and CLK='1' then  
-			reg <= reg(16-2 downto 0) & fb;
+		if int_clk1'event and int_clk1='1' then  
+			reg1 <= reg1(16-2 downto 0) & fb1;
 		end if;
 	end process;
-	OUTPUT <= fb;
+	OUTPUT1 <= fb1 when OE1 = '1' else '0';
+
+	process(reg2, INPUT2, tap_reg) is
+		variable tmp : std_logic;
+	begin
+	 tmp := INPUT2;
+    for I in 15 downto 0 loop
+        tmp := tmp xor (reg2(I) and tap_reg(I));
+    end loop;
+    fb2 <= tmp;
+	end process;
+
+	process (CLK2)
+	begin
+		if CLK2'event and CLK2='1' then  
+			reg2 <= reg2(16-2 downto 0) & fb2;
+		end if;
+	end process;
+	OUTPUT2 <= fb2 when OE2 = '1' else '0';	
+	
+	process (SCK)
+	begin 
+		if SCK'event and SCK='1' then  
+			tap_reg <= tap_reg(16-2 downto 0) & SDATA;
+		end if;
+	end process;
+		
 	
 end Behavioral;
 
