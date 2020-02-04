b911cbc1b5322d24e17ee8a7cd0d3c3b55c96f93
wlaub <goldcray@gmail.com>
2020-02-03 18:52:02 -0800

    Added IO expander

--- PrometheusII/CPLD/main.vhd
+++ PrometheusII/CPLD/main.vhd
@@ -6,7 +6,7 @@
 -- Design Name: 
 -- Module Name:    main - Behavioral 
 -- Project Name: 
--- Target Devices: 
+-- Target DeviWEs: 
 -- Tool versions: 
 -- Description: 
 --
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -31,78 +32,95 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity main is
     Port ( 
-	        CLK1 : in  STD_LOGIC;
-           INPUT1 : in  STD_LOGIC;
-           OUTPUT1 : out  STD_LOGIC;
-			  OE1 : in  STD_LOGIC;
+	        CLK0 : in  STD_LOGIC;
+           INPUT0 : in  STD_LOGIC;
+           OUTPUT0 : out  STD_LOGIC;
 			  
 			  EXT_CLK : in STD_LOGIC;
 			  CLK_SEL : in STD_LOGIC;
 
-			  CLK2 : in  STD_LOGIC;
-           INPUT2 : in  STD_LOGIC;
-           OUTPUT2 : out  STD_LOGIC;
-			  OE2 : in  STD_LOGIC;		  
+			  CLK1 : in  STD_LOGIC;
+           INPUT1 : in  STD_LOGIC;
+           OUTPUT1 : out  STD_LOGIC;  
 			  
+           IO : in std_logic_vector(6 downto 0);
+           
+			  WE : in STD_LOGIC;
+			  SDO : out STD_LOGIC;
 			  SCK : in STD_LOGIC;
-			  SDATA : in STD_LOGIC
+			  SDI : in STD_LOGIC
            );			  
 end main;
 
 
 architecture Behavioral of main is
-	signal reg1: std_logic_vector(15 downto 0);
-	signal reg2: std_logic_vector(15 downto 0);
-	signal tap_reg: std_logic_vector(15 downto 0);
-	signal fb1: STD_LOGIC;
-	signal fb2: STD_LOGIC;
-	signal int_clk1: STD_LOGIC;
-begin
+   constant LEN : integer := 11; --actual length - 1 for convenienWE
+   constant SLEN : integer := 11;
 
+	signal reg0: std_logic_vector(LEN downto 0) := (others => '0');
+	signal reg1: std_logic_vector(LEN downto 0) := (others => '0');
+	signal tap_reg: std_logic_vector(LEN+2 downto 0) := (others => '0');
+	signal fb0: STD_LOGIC;
+	signal fb1: STD_LOGIC;
+	signal int_CLK0: STD_LOGIC;
+	signal out_reg: std_logic_vector(SLEN downto 0) := (others => '0');
+	signal status_reg: std_logic_vector(SLEN downto 0);
+   signal oe: std_logic_vector(1 downto 0);
+begin
 	
+	process(reg0, INPUT0, tap_reg) is
+		variable tmp : std_logic;
+	begin
+	 tmp := INPUT0;
+    for I in LEN downto 0 loop
+        tmp := tmp xor (reg0(I) and tap_reg(I));
+    end loop;
+    fb0 <= tmp;
+	end process;
+				
+   int_CLK0 <= CLK0 when CLK_SEL = '0' else EXT_CLK;
+	process (int_CLK0)
+	begin
+		if OE(0) = '1' and int_CLK0'event and int_CLK0='1' then  
+			reg0 <= reg0(LEN-1 downto 0) & fb0;
+		end if;
+	end process;
+	OUTPUT0 <= fb0 when OE(0) = '1' else '0';
+
 	process(reg1, INPUT1, tap_reg) is
 		variable tmp : std_logic;
 	begin
 	 tmp := INPUT1;
-    for I in 15 downto 0 loop
+    for I in LEN downto 0 loop
         tmp := tmp xor (reg1(I) and tap_reg(I));
     end loop;
     fb1 <= tmp;
 	end process;
-				
-   int_clk1 <= CLK1 when CLK_SEL = '0' else EXT_CLK;
-	process (int_clk1)
+
+	process (CLK1)
 	begin
-		if int_clk1'event and int_clk1='1' then  
-			reg1 <= reg1(16-2 downto 0) & fb1;
+		if OE(1) = '1' and CLK1'event and CLK1='1' then  
+			reg1 <= reg1(LEN-1 downto 0) & fb1;
 		end if;
 	end process;
-	OUTPUT1 <= fb1 when OE1 = '1' else '0';
+	OUTPUT1 <= fb1 when OE(1) = '1' else '0';	
 
-	process(reg2, INPUT2, tap_reg) is
-		variable tmp : std_logic;
-	begin
-	 tmp := INPUT2;
-    for I in 15 downto 0 loop
-        tmp := tmp xor (reg2(I) and tap_reg(I));
-    end loop;
-    fb2 <= tmp;
-	end process;
-
-	process (CLK2)
-	begin
-		if CLK2'event and CLK2='1' then  
-			reg2 <= reg2(16-2 downto 0) & fb2;
-		end if;
-	end process;
-	OUTPUT2 <= fb2 when OE2 = '1' else '0';	
-	
-	process (SCK)
+	process (SCK, WE)
 	begin 
-		if SCK'event and SCK='1' then  
-			tap_reg <= tap_reg(16-2 downto 0) & SDATA;
-		end if;
+	  if WE='1' then  
+       out_reg(10 downto 4) <= IO;
+		 out_reg(11) <= CLK_SEL;
+	    if SCK'event and SCK='1' then  
+         tap_reg <= tap_reg(LEN+1 downto 0) & SDI;	
+       end if;
+     else
+       oe <= tap_reg(LEN+2 downto LEN+1);
+	    if SCK'event and SCK='1' then  
+         out_reg <= out_reg(SLEN-1 downto 0) & '0';
+       end if;
+     end if;
 	end process;
+	SDO <= out_reg(SLEN);
 		
 	
 end Behavioral;
