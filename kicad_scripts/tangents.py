917576f902ae3891d3502141cdadfbbddef64e9c
wlaub <goldcray@gmail.com>
2020-04-07 17:16:30 -0700

    Move tangents script

--- kicad_scripts/tangents.py
+++ kicad_scripts/tangents.py
@@ -98,6 +98,7 @@ def get_inner_tangents(r1, x1, y1, r2, x2, y2):
     the two given circles. Return also the corresponding angles on each circle.
     Ref http://www.ambrsoft.com/TrigoCalc/Circles2/Circles2Tangent_.htm
     [[[x, y], [x,y]] , [[x,y], [x,y]]], [[a1, a1], [a2,a2]]
+    First line is under first circle, second is over?
     """
 
     xp = (x2*r1-x1*r2)/(r1+r2)
@@ -106,8 +107,25 @@ def get_inner_tangents(r1, x1, y1, r2, x2, y2):
     return get_tangents(xp, yp, r1, x1, y1, r2, x2, y2)
 
 
+def get_trace(dx, dy, r1, r2, over1, over2):
+    """
+    Given a circle of radius r1 at 0,0
+    A circle of radius r2 at dx, dy
+    over1 = 0 if the tangent is under r1
+    over1 = 1 if the tangent is over r1
+    over2 = 0 if the tangent is under r2
+    over2 = 1 if the tangent is over r2
+    """
+    if over1 != over2:
+        lines, angles = get_inner_tangents(r1, 0, 0, r2, dx, dy)
+        return lines[1-over1], [angles[0, 1-over1], angles[1,1-over1]]
+    else:
+        lines, angles = get_outer_tangents(r1, 0, 0, r2, dx, dy)
+        return lines[over1], [angles[0, over1], angles[1,over1]]
 
-test = [1,0,0,2,3,0]
+
+
+test = [1,0,0,2,4,0]
 print(test)
 print(get_outer_tangents(*test))
 print(get_inner_tangents(*test))
