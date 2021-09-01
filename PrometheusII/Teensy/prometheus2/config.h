175e975055760d480965f6521afb43c825f3e73c
wlaub <goldcray@gmail.com>
2021-08-31 18:59:26 -0700

    Added missing config.h

---
+++ PrometheusII/Teensy/prometheus2/config.h
@@ -0,0 +1,32 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#pragma once
+
+struct PitchConfig 
+{
+  signed char octave = 0;
+  signed char semitone = 0;
+  unsigned char enabled = 0;
+  float semitone_val = 0;
+};
+
+#define RESET_PITCH_CONFIG(_CONFIG)\
+  _CONFIG.octave = 0;\
+  _CONFIG.semitone = 0;\
+  _CONFIG.semitone_val = 0;\
+
+#define SET_PITCH_CONFIG_SEMITONE(_CONFIG, _VAL)\
+  _CONFIG.semitone = _VAL;\
+  _CONFIG.semitone_val = float(_VAL)/12.0;\
+
+#define INC_PITCH_CONFIG_SEMITONE(_CONFIG)\
+  ++_CONFIG.semitone;\
+  _CONFIG.semitone_val = float(_CONFIG.semitone)/12.0;\
+
+#define DEC_PITCH_CONFIG_SEMITONE(_CONFIG)\
+  --_CONFIG.semitone;\
+  _CONFIG.semitone_val = float(_CONFIG.semitone)/12.0;\
+
+  
+#endif
