a2110ced7f84671e75b5c54a4afd6240728f82de
wlaub <goldcray@gmail.com>
2021-09-08 18:09:59 -0700

    updated plotting script, add baseline data

--- PrometheusII/scripts/decay_plot.py
+++ PrometheusII/scripts/decay_plot.py
@@ -11,6 +11,10 @@ filenames = []
 for path in sys.argv[1:]:
     filenames.extend(glob.glob(path))
 
+xlim = [1000,-1000]
+
+bias_remap = False
+
 for filename in filenames:
 
     with open(filename, 'r') as fp:
@@ -24,14 +28,22 @@ for filename in filenames:
         return 1.037594*x + .04203
 
     xvals = [(x['bias_min']+x['bias_max'])/2 for x in data]
-    #xvals = [bias_map(x) for x in xvals]
+    if bias_remap:
+        xvals = [bias_map(x) for x in xvals]
     yvals = [x['duration']*1000 for x in data]
 
-    plt.scatter(xvals, yvals, s=1, label=label)
+    xlim[0] = min(xlim[0], min(xvals))
+    xlim[1] = max(xlim[1], max(xvals))
+
+    plt.scatter(xvals, yvals, s=1, label=label, alpha = 0.5)
 
 plt.title('Measured filter decay times')
-plt.xlabel('Bias Voltage (V)')
+if bias_remap:
+    plt.xlabel('Bias Voltage (V)')
+else:
+    plt.xlabel('Unmapped Bias Voltage (V)')
 plt.ylabel('Decay Time (ms)')
+plt.xlim(*xlim)
 plt.legend()
 plt.grid(True)
 ax = plt.gca()
